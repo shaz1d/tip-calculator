1b67458d4466e1ca4a753c904bb8bdef30ae32ce
Shazidul Islam <47906835+shaz1d@users.noreply.github.com>
2023-10-05 17:35:19 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="w-full min-h-screen bg-pest-300 flex flex-col justify-center items-center">
+      <h1 className="uppercase text-2xl text-center font-bold text-pest-600 tracking-[10px]">
+        Spli
+        <br />
+        tter
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(172, 67%, 45%)",
+        pest: {
+          200: "hsl(189, 41%, 97%)",
+          300: "hsl(185, 41%, 84%)",
+          400: "hsl(184, 14%, 56%)",
+          450: "hsl(173, 61%, 77%)",
+          500: "hsl(186, 14%, 43%)",
+          600: "hsl(183, 100%, 15%)",
+        },
+      },
+      boxShadow: {
+        "3xl": "0 20px 30px 0px rgba(0, 0, 0, 0.05)",
+      },
+    },
+  },
+  plugins: [],
+};
