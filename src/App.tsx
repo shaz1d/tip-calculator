8a036757ad0762aa41fa17d8eab0ea4c959fb497
Shazid <shazid.dev@gmail.com>
2023-10-09 12:00:06 +0600

    Container Card added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import ContainerCard from "./components/cards/ContainerCard";
+
 function App() {
   return (
     <div className="w-full min-h-screen bg-pest-300 flex flex-col justify-center items-center">
-      <h1 className="uppercase text-2xl text-center font-bold text-pest-600 tracking-[10px]">
+      <h1 className="uppercase text-2xl text-center font-bold text-pest-600 tracking-[10px] mb-10">
         Spli
         <br />
         tter
       </h1>
+      <ContainerCard />
     </div>
   );
 }
---
+++ src/components/cards/ContainerCard/ContainerCard.tsx
@@ -0,0 +1,11 @@
+type ContainerCardProps = {};
+
+const ContainerCard = () => {
+  return (
+    <div className="max-w-4xl p-5 grid grid-cols-2 bg-white rounded-3xl shadow-2xl">
+      splitter
+    </div>
+  );
+};
+
+export default ContainerCard;
